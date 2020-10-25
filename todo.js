a76bafca03a207582ec69e701c7bdf3a8ffb6cff
Tom <tom.tinel@gmail.com>
2020-10-25 18:09:59 +0000

    reorganised all the logic

--- todo.js
+++ todo.js
@@ -1,38 +1,54 @@
-const todos = [];
 
-const key = 'ListOfTodos';
-const input = document.querySelector('#toDoText')
-const newLi = document.createElement('li') // id ?
-const targetUl = document.querySelector('#listToDo')
+const KEY = 'ListOfTodos';
 
-    var finalTodos = localStorage.getItem(key);
-    finalTodos = JSON.parse(finalTodos)
+// checks what's in localstorage and puts it in a array
+const getData = () => {
+    let data = localStorage.getItem(KEY)
+    data = JSON.parse(data)
+    return Array.from(data); // pb here - object null isn't iterable
+}
 
-    for(i=0; i<finalTodos.length; i++){
-        newLi.innerHTML = finalTodos[i];   
-        targetUl.append(newLi); 
+// checks for new input, pushes into our array, stringify it and add set to localstorage
+const setData = () => {
+    const input = document.querySelector('#toDoText')
+    const data = getData()
+    if(input.value != undefined || input.value != ''){
+        data.push(input.value)
+        const valueJSON = JSON.stringify(data)
+        localStorage.setItem(KEY, valueJSON)
     }
+}
 
-const createToDo = (key, input, newLi, targetUl) => {
-    event.preventDefault()
-
-    if(input.value == ''){
-        return console.log('input empty you silly');
+// creates new li - (data) should be our array
+const displayData = (data) => {
+    const targetUl = document.querySelector('#listToDo')
+    if(data != null){
+        for(i=0; i<data.length; i++) {
+            let newLi = document.createElement('li')
+            newLi.innerHTML = data[i]
+            targetUl.append(newLi)
+        }
     }
-    
-    todos.push(input.value);
-    const strTodos = JSON.stringify(todos);
-    localStorage.setItem(key, strTodos);
-     var finalTodos = localStorage.getItem(key);
-    finalTodos = JSON.parse(finalTodos)
+}
 
-    for(i=0; i<finalTodos.length; i++){
-        newLi.innerHTML = finalTodos[i];   
-        targetUl.append(newLi); 
-    }
-    
-    input.value = '';
-};
+const onSubmit = () => {
+    setData();
+    var state = getData();
+    displayData(state);
+}
+
+const init = () => {
+    var state = getData();
+    displayData(state);
+}
+
+init();
+
+//     for(i=0; i<finalTodos.length; i++){
+//         newLi.innerHTML = finalTodos[i];   
+//         targetUl.append(newLi); 
+//     }
+// }
 
 // li should have a hover (CSS) with a function to delete (-) (JS id? sur qui on a cliqué)
 
