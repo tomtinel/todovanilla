7888ff2556ecb2181d839e5c284f485b9f861f67
Tom <tom.tinel@gmail.com>
2020-10-25 22:49:41 +0000

    delete option, toggle with a bug

--- todo.js
+++ todo.js
@@ -1,11 +1,16 @@
-
 const KEY = 'ListOfTodos';
 
 // checks what's in localstorage and puts it in a array
 const getData = () => {
     let data = localStorage.getItem(KEY)
     data = JSON.parse(data)
-    return Array.from(data); // pb here - object null isn't iterable
+    if(data == null){
+        data = []
+    }
+    else {
+        Array.from(data)
+    }
+    return data
 }
 
 // checks for new input, pushes into our array, stringify it and add set to localstorage
@@ -13,7 +18,10 @@ const setData = () => {
     const input = document.querySelector('#toDoText')
     const data = getData()
     if(input.value != undefined || input.value != ''){
-        data.push(input.value)
+        data.push({
+            content: input.value,
+            status: 'active',
+        })
         const valueJSON = JSON.stringify(data)
         localStorage.setItem(KEY, valueJSON)
     }
@@ -22,41 +30,71 @@ const setData = () => {
 // creates new li - (data) should be our array
 const displayData = (data) => {
     const targetUl = document.querySelector('#listToDo')
+            targetUl.innerHTML = ''
     if(data != null){
         for(i=0; i<data.length; i++) {
             let newLi = document.createElement('li')
-            newLi.innerHTML = data[i]
+            let cross = document.createElement('div')
+
+            newLi.id = i
+            newLi.classList.add(data[i].status)
+            newLi.innerHTML = data[i].content
+            newLi.onclick = (event) => {
+                toggle(event)
+            }
+            
+            cross.innerHTML = 'X'
+            cross.classList.add('deleteButton')
+            cross.dataset.idli = i
+            cross.onclick = (event) => {
+                onDelete(event)
+            }
+            
+            newLi.append(cross)
             targetUl.append(newLi)
         }
-    }
+      }
 }
 
+const toggle = () => {
+    const id = event.currentTarget.id
+    let data = getData()
+    if(data[id].status == 'active'){
+        data[id].status = 'completed'       
+    }else{
+        data[id].status = 'active'
+    }
+    const valueJSON = JSON.stringify(data)
+    localStorage.setItem(KEY, valueJSON)
+    displayData(data)
+}
+
+const onDelete = (event) => {
+    const id = event.currentTarget.dataset.idli
+    let data = getData()
+        data.splice(id, 1)
+        console.log(data);
+    const valueJSON = JSON.stringify(data)
+    localStorage.setItem(KEY, valueJSON)
+    displayData(data)
+    }
+
 const onSubmit = () => {
+    event.preventDefault()
     setData();
-    var state = getData();
-    displayData(state);
+    var state = getData()
+    displayData(state)
 }
 
 const init = () => {
-    var state = getData();
-    displayData(state);
+    var state = getData()
+    displayData(state)
 }
 
-init();
-
-//     for(i=0; i<finalTodos.length; i++){
-//         newLi.innerHTML = finalTodos[i];   
-//         targetUl.append(newLi); 
-//     }
-// }
+init()
 
 // li should have a hover (CSS) with a function to delete (-) (JS id? sur qui on a cliqué)
 
-//localstorage (or Cookie ?) for persistant data
-
-
 // Use a filter on an array (status: active or completed) - 
-
-// Localstorage JSON stringify, and parse for save and load, use a todosService as an interface, turn data into an array
 
 // example: http://todomvc.com/examples/react/#/
